e52c0cfa52d343bd30bc652797e62504feb658ea
Marco <marco.baeuml@icloud.com>
2021-06-26 02:20:43 +0200

    added some stuff

--- index.js
+++ index.js
@@ -1,58 +1,98 @@
 process.env.NTBA_FIX_319 = 1;
 process.env.NTBA_FIX_350 = 1;
-const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
-const dateFormater = require('date-and-time');
-const glob = require("glob")
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs-extra");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const dateFormater = require("date-and-time");
 
-const token = '1898235019:AAFWytR_4QaPP_zjkSIXwcaGoKBO3zzTD4E';
-const bot = new TelegramBot(token, {polling: true});
-let date = new Date();
-date = dateFormater.format(date, 'YYYY-MM-DD_HH-mm');
-let filename;
-let option;
-let validCommand = true;
-let url;
+const token = "1898235019:AAHK8TEyGfZOJqnHEzH-qWMNhhni8hmudew";
+const bot = new TelegramBot(token, { polling: true });
+let longdate = new Date();
+date = dateFormater.format(longdate, "YYYY-MM-DD_HH-mm");
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  switch (msg.text) {
-    case "/desktop":
-      validCommand = true;
-      option = "desktop";
+bot.on("message", (msgjson) => {
+  const chatId = msgjson.chat.id;
+  const msg = msgjson.text;
+  switch (true) {
+    case msg == "/help":
+      onHelp(chatId);
       break;
-    case "/mobile":
-      validCommand = true;
-      option = "mobile";
+    case /url(.*)/.test(msg):
+      onUrl(chatId, msg);
+      break;
+    case msg == "/generate":
+      onGenerate(chatId);
       break;
     default:
-      validCommand = false;
-      const url = msg.text.split(' ');
-      if (url[0] == "/url") {
-        fs.writeFileSync("url.txt", url[1]);
-      }else{
-        bot.sendMessage(chatId, "Invalid Command");
-      }
+      bot.sendMessage(chatId, "Invalid Command");
       break;
   }
-  if (validCommand) {
-    bot.sendMessage(chatId, "Lighthouse is warming up...");
-    url = fs.readFileSync('url.txt', 'utf8')
-    filename = option+"_"+date+".html";
-    executeLighthouse().then(()=>{
-      bot.sendDocument(chatId, filename); 
-      fs.unlinkSync(filename);
-    });
-  };
 });
 
-async function executeLighthouse(){
-  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-  const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance', 'accessibility','best-practices', 'seo'], port: chrome.port};
-  const runnerResult = await lighthouse(url, options);
+bot.on("callback_query", (callbackQuery) => {
+  const chatId = callbackQuery.from.id;
+  const device = callbackQuery.data;
+  const filename = device + "-" + date + ".html";
+  generateLighthouse(chatId, filename, device).then(() => {
+    sendLighthouseReport(chatId, filename);
+  });
+});
+
+function onHelp(chatId) {
+  bot.sendMessage(
+    chatId,
+    "/help - print help\n /url - display current url\n /url <url> - change url\n /generate - generate Lighthouse report"
+  );
+}
+
+function onUrl(chatId, msg) {
+  const urlCommandSplitted = msg.split(" ");
+  if (urlCommandSplitted.length == 1) {
+    const urlJason = fs.readJsonSync("url.json");
+    bot.sendMessage(chatId, urlJason.url);
+  } else if (urlCommandSplitted.length == 2) {
+    fs.outputJsonSync("url.json", { url: urlCommandSplitted[1] });
+  }
+}
+
+function onGenerate(chatId) {
+  const opts = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Desktop",
+            callback_data: "desktop",
+          },
+          {
+            text: "Mobile",
+            callback_data: "mobile",
+          },
+        ],
+      ],
+    },
+  };
+  bot.sendMessage(chatId, "Select device for lighthouse report ", opts);
+}
+
+async function generateLighthouse(chatId, filename, device) {
+  bot.sendMessage(chatId, "Lighthouse is warming up...");
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const options = {
+    logLevel: "info",
+    output: "html",
+    onlyCategories: ["performance", "accessibility", "best-practices", "seo"],
+    port: chrome.port,
+  };
+  const urlJason = await fs.readJsonSync("url.json");
+  const runnerResult = await lighthouse(urlJason.url, options);
   const reportHtml = runnerResult.report;
   fs.writeFileSync(filename, reportHtml);
   await chrome.kill();
-};
+}
+
+async function sendLighthouseReport(chatId, filename) {
+  await bot.sendDocument(chatId, filename);
+  fs.unlinkSync(filename);
+}
